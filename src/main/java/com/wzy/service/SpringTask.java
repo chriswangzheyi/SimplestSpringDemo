767184d4a094f3f29f4af99aa96c454fc6b3d3ee
zheyi <chriswangzheyi@hotmail.com>
2017-12-11 09:36:32 +0800

    spring timer
    spring timer

---
+++ src/main/java/com/wzy/service/SpringTask.java
@@ -0,0 +1,19 @@
+package com.wzy.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component("springTask")
+
+public
+class  SpringTask {
+
+    @Scheduled(cron = "0/2 * * * * ?")
+
+    public
+void  myTask() {
+
+        System.out.println("这个任务两秒执行一次！");
+
+    }
+}
