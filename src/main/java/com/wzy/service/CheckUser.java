d79f65ffca85597c51dc5982f1cb279bfb27afda
zheyi <chriswangzheyi@hotmail.com>
2018-03-19 16:02:53 +0800

    spring aop
    spring aop

--- src/main/java/com/wzy/service/CheckUser.java
+++ src/main/java/com/wzy/service/CheckUser.java
@@ -6,18 +6,18 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
   
-@Aspect
-@Component
+@Aspect    //Aspct注解
+@Component  //必须加上compoent,否则切面不会执行
 public class CheckUser {  
   
     @Pointcut("execution(* com.wzy.service.*.find*(..))")  
     public void checkUser(){  
-        System.out.println("**************The System is Searching Information For You****************");  
+        System.out.println("**************The System is Searching Information For You****************");   //不会被执行
     }  
       
     @Pointcut("execution(* com.wzy.service.*.add*(..))")  
     public void checkAdd(){  
-        System.out.println("**************<< Add User >> Checking.....***************");  
+        System.out.println("**************<< Add User >> Checking.....***************");    //不会被执行
     }  
       
     @Before("checkUser()")  
